486276757b205da7bc5099080a8741a67d84d536
gonghr <13962217@qq.com>
2018-07-30 17:54:14 +0800

    查询产品线和产品线包含的产品

--- apps/maintenance/web/src/main/java/net/aicoder/devp/maintenance/business/product/product/controller/DevpPrdPrdlineController.java
+++ apps/maintenance/web/src/main/java/net/aicoder/devp/maintenance/business/product/product/controller/DevpPrdPrdlineController.java
@@ -6,14 +6,16 @@ import com.yunkang.saas.common.framework.web.data.PageSearchRequest;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
-import net.aicoder.devp.product.business.product.dto.DevpPrdPrdlineCondition;
-import net.aicoder.devp.product.business.product.dto.DevpPrdPrdlineAddDto;
-import net.aicoder.devp.product.business.product.dto.DevpPrdPrdlineEditDto;
+import net.aicoder.devp.maintenance.business.product.product.service.DevpPrdLinePrdRibbonService;
+import net.aicoder.devp.maintenance.business.product.product.service.DevpPrdProductRibbonService;
+import net.aicoder.devp.product.business.product.dto.*;
+import net.aicoder.devp.product.business.product.vo.DevpPrdLinePrdVO;
 import net.aicoder.devp.product.business.product.vo.DevpPrdPrdlineVO;
 import net.aicoder.devp.maintenance.business.product.product.service.DevpPrdPrdlineRibbonService;
 import net.aicoder.devp.maintenance.business.product.product.valid.DevpPrdPrdlineValidator;
 
 
+import net.aicoder.devp.product.business.product.vo.DevpPrdProductVO;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeanUtils;
@@ -41,6 +43,12 @@ public class DevpPrdPrdlineController {
 
 	@Autowired
 	private DevpPrdPrdlineRibbonService devpPrdPrdlineRibbonService;
+
+	@Autowired
+	private DevpPrdProductRibbonService devpPrdProductRibbonService;
+
+	@Autowired
+	private DevpPrdLinePrdRibbonService devpPrdLinePrdRibbonService;
 
 	@Autowired
 	DevpPrdPrdlineValidator devpPrdPrdlineValidator;
@@ -125,5 +133,46 @@ public class DevpPrdPrdlineController {
 
 	}
 
+	/**
+	 * 查询产品线
+	 * @param pageSearchRequest
+	 * @return
+	 */
+	@ApiOperation(value = "查询", notes = "根据条件查询产品线及包含的产品", httpMethod = "POST")
+	@PostMapping("/tree")
+	public PageContent<DevpPrdPrdlineVO> tree(@RequestBody PageSearchRequest<DevpPrdPrdlineCondition> pageSearchRequest){
+
+		PageContent<DevpPrdPrdlineVO> pageContent = devpPrdPrdlineRibbonService.list(pageSearchRequest);
+
+		for(DevpPrdPrdlineVO lineVo : pageContent.getContent()){
+			List<DevpPrdProductVO> productVOList = this.findProductListForLine(lineVo.getId());
+			lineVo.setProductList(productVOList);
+		}
+
+
+		LOGGER.debug("page Size :{}, total:{}", pageContent.getContent().size() ,pageContent.getTotal());
+		return pageContent;
+
+	}
+
+	private List<DevpPrdProductVO> findProductListForLine(Long lineId){
+		DevpPrdLinePrdCondition condition = new DevpPrdLinePrdCondition();
+		condition.setLineRid(lineId);
+
+		PageSearchRequest<DevpPrdLinePrdCondition> productCondition = new PageSearchRequest<>();
+		productCondition.setSearchCondition(condition);
+		productCondition.setLimit(10000);
+		productCondition.setPage(0);
+
+		PageContent<DevpPrdLinePrdVO>  devpPrdLinePrdVOPageContent = this.devpPrdLinePrdRibbonService.list(productCondition);
+
+		List<DevpPrdProductVO> result = new ArrayList<>();
+		for(DevpPrdLinePrdVO vo : devpPrdLinePrdVOPageContent.getContent()){
+			DevpPrdProductVO productVO = this.devpPrdProductRibbonService.find(vo.getPrdRid());
+			result.add(productVO);
+		}
+
+		return result;
+	}
 
 }
