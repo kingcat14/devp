8dbaf38eadb866921fd0589fb77bde52a5f1b53d
gonghr <13962217@qq.com>
2018-10-31 13:33:08 +0800

    新增自动初始化用户ID的注解

---
+++ backend/bootstrap-parent/application/src/main/java/com/yunkang/saas/bootstrap/application/business/annotation/PersonalAnnotation.java
@@ -0,0 +1,18 @@
+package com.yunkang.saas.bootstrap.application.business.annotation;
+
+import java.lang.annotation.*;
+
+@Target({ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Inherited
+public @interface PersonalAnnotation {
+
+
+    boolean autoCondition() default true;
+
+    /**
+     * (可选) 如果用在查询请求上, 并且设置了条件类的class,当查询条件为空的时候，会默认创建一个条件对象
+     */
+    Class conditionClass() default Object.class;
+}
